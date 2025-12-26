3abba18611a41810f5c14cca7b53e68418c53f52
PhamNghia11 <phamlenghiadx2@gmail.com>
2025-12-26 14:16:13 +0700

    Fix job visibility cache issue

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -22,6 +22,8 @@ async function getActiveJobsFromDB(): Promise<Job[]> {
   }
 }
 
+export const dynamic = 'force-dynamic'
+
 export default async function JobsPage() {
   const dbJobs = await getActiveJobsFromDB()
 
