2edf448bf2ac191e130b2d0501075f5a5de82c6d
PhamNghia11 <phamlenghiadx2@gmail.com>
2026-01-25 16:48:46 +0700

    fix: exhaustive remotePatterns for YouTube and Unsplash images

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,26 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'img.youtube.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i1.ytimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i2.ytimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i3.ytimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i4.ytimg.com',
+      },
     ],
   },
   async redirects() {
