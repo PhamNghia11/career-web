2efcbcea104d7351db258c5eeefe05722393609c
PhamNghia11 <phamlenghiadx2@gmail.com>
2026-02-01 10:08:44 +0700

    Adjust banner content vertical position: shift down by 1cm (inc padding to pt-36/48)

--- src/components/home/hero-section.tsx
+++ src/components/home/hero-section.tsx
@@ -110,7 +110,7 @@ export function HeroSection() {
           <div className="absolute inset-0 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${slide.image})` }}>
             <div className="absolute inset-0 bg-black/20" />
           </div>
-          <div className="relative h-full min-h-[80vh] lg:min-h-[85vh] container mx-auto px-4 flex flex-col justify-start pt-32 lg:pt-40">
+          <div className="relative h-full min-h-[80vh] lg:min-h-[85vh] container mx-auto px-4 flex flex-col justify-start pt-36 lg:pt-48">
             <div className="max-w-3xl">
               {/* Title with fixed height space for 2 lines */}
               <div className="min-h-[100px] md:min-h-[140px] flex items-end mb-4">
