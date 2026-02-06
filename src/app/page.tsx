c1c68af58ab40d813709f1bf4c106cf480fd3c9b
PhamNghia11 <phamlenghiadx2@gmail.com>
2026-02-06 10:41:07 +0700

    fix: revert homepage featured jobs limit to 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export const revalidate = 3600 // Revalidate home page every 1 hour
 export default async function HomePage() {
   const [slides, jobs, featuredJobsConfig] = await Promise.all([
     getHeroSlides("home"),
-    getLatestJobs(8),
+    getLatestJobs(4),
     getSiteConfig("home_featured_jobs")
   ])
 
